f81a38fa842149baa356aab5d5584191305a0002
starius37 <okm50088@gmail.com>
2016-01-09 16:45:09 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, request, redirect
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
+import flask.ext.login as flask_login
+
+app = Flask(__name__)  # class ClassName() -> Class Instance (Functional)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.secret_key = 'secret_key_random'  #
+
+db = SQLAlchemy(app)  # DB Connection Instance
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+from flask.ext.misaka import Misaka
+Misaka(app)
+
+from models import *
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/auth/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('auth/login.html')
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.verify_password(password):
+                flask_login.login_user(user)
+                return redirect('/')
+            else:
+                return redirect('/')
+        else:
+            return redirect('/')
+
+@app.route('/auth/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('auth/signup.html')
+
+    if request.method == 'POST':
+        user = User(email=request.form['email'])
+        user.set_password(request.form['password'])
+        user.role = 'user'
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/auth/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect('/')
+
+
+@app.route('/')  # Request -> Routing , View(Controller) -> Response
+def index():
+    posts = Post.query.all()
+    return render_template('list_post.html', posts=posts)
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    if request.method == 'GET':
+        return render_template('write_post.html')
+
+    if request.method == 'POST':
+        post = Post(
+            title=request.form['title'],
+            content=request.form['content']
+        )
+        db.session.add(post)
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/post/<int:id>', methods=['GET'])
+def post_detail(id):
+    if request.method == 'GET':
+        post = Post.query.get(id)
+        return render_template('detail_post.html', post=post)
+
+@app.route('/post/update/<int:id>', methods=['GET', 'POST'])
+def post_update(id):
+    if request.method == 'GET':
+        post = Post.query.get(id)
+        return render_template('update_post.html', post=post)
+    if request.method == 'POST':
+        post = Post.query.get(id)
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/post/delete/<path:id>', methods=['POST'])
+def post_delete(id):
+    if request.method == 'POST':
+        post = Post.query.get(id)
+        db.session.delete(post)
+        db.session.commit()
+        return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,36 @@
+from app import db
+from datetime import datetime
+import flask.ext.login as flask_login
+from flask.ext.bcrypt import Bcrypt
+
+class User(db.Model, flask_login.UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+    role = db.Column(db.String(20), default="user")
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def get_id(self):
+        return self.id
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
+    def set_password(self, password):
+        bcrypt = Bcrypt()
+        self.password = bcrypt.generate_password_hash(password, 12)
+
+    def verify_password(self, password):
+        bcrypt = Bcrypt()
+        return bcrypt.check_password_hash(self.password, password)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    updated_at = db.Column(db.DateTime, nullable=True, onupdate=datetime.utcnow())
